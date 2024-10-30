1e1b5108d083299839b643dec2186ecc9a90598a
hyounshik lim <tubesoak@gmail.com>
2024-10-30 22:43:53 +0900

    1-8.py

---
+++ 1-8.py
@@ -0,0 +1,6 @@
+from sympy import *
+
+x = symbols('x')
+f = x**2 + 1
+
+plot(f)
---
+++ 1-9.py
@@ -0,0 +1,7 @@
+from sympy import *
+from sympy.plotting import plot3d
+
+x, y = symbols('x y')
+f = 2*x + 3*y
+
+plot3d(f)
